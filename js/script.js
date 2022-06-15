056bf37d8ed2e33c74f24d324d1b71dfdf5e1c33
Gabriel <gabrielcarvalho@poli.ufrj.br>
2022-06-14 23:58:54 -0300

    Add function that plays a single round

--- js/script.js
+++ js/script.js
@@ -1,6 +1,44 @@
 function computerPlay() {
-    let computerOptions = ['Rock', 'Paper', 'Scissors'];
-    return computerChoice = computerOptions[Math.floor(Math.random()*3)];
+    let computerOptions = ['rock', 'paper', 'scissors'];
+    return computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
+            return 'You Win! Rock beats Scissors';
+        } 
+        else if (computerSelection === 'paper') {
+            return 'You Lose! Paper beats Rock';
+        } else {
+            return 'It\'s a Tie!';
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            return 'You Win! Paper beats Rock';
+        } else if (computerSelection === 'scissors') {
+            return 'You Lose! Scissors beats Paper';
+        } else {
+            return 'It\'s a Tie';
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            return 'You Win! Scissors beats Paper';
+        } else if (computerSelection === 'rock') {
+            return "You Lose! Rock beats Scissors";
+        } else {
+            return 'It\'s a Tie';
+        }
+    } else {
+        return 'Choose between "rock", "paper" or "scissors"!';
+    }
+}
+
+const playerSelection = 'sCIssOrs';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
+// Test selections
+console.log(`You selected: ${playerSelection.toLowerCase()}\nComputer selected: ${computerSelection}`);
